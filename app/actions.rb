c70cab34cfb96061f06f2fed961d547c23e3d031
vagrant <vagrant@lighthouse-labs>
2014-05-28 20:44:34 +0000

    new commit post lunch

--- app/actions.rb
+++ app/actions.rb
@@ -16,3 +16,26 @@ get '/places/:id' do
   @place = Place.find params[:id]
   erb :'places/show'
 end
+
+get '/login' do
+  erb :'auth/login'
+end
+
+get '/signup' do
+  erb :'auth/signup'
+end
+
+post '/login' do
+  # @user = User.where(email: params[:email], password: params[:password]).first
+  #
+  # @user = User.where("users.email = ? AND users.password=?", params[:email], params[:password]).first
+
+  @user = User.find_by(email: params[:email], password: params[:password])
+
+  if @user
+    session[:user_id] = @user.id
+    redirect '/places/new'
+  else
+    erb :'auth/login'
+  end
+end
