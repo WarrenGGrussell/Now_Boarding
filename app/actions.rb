afd01c59bf0fffd7073b5db68dd0f901dba1e269
Romila Barryman <romila.barryman@gmail.com>
2014-05-29 19:01:33 +0000

    Actions Debugged. Countries List Deleted. Profile Page Added.

--- app/actions.rb
+++ app/actions.rb
@@ -1,32 +1,24 @@
+helpers do
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+end
+
 # Homepage (Root path)
 get '/' do
   erb :index
 end
 
 get '/countries' do
-  @countries = Country.all
+    @countries = Country.all
   erb :'countries/index'
 end
 
-get 'countries/new' do
-  erb :'countries/new'
-end
-
-get '/countries/:id' do
-  @country = Country.find params[:id]
-  erb :'countries/show'
-end
-
 post '/countries' do
-  @country = Country.new(
-    name: params[:name]
-  )
-  if @country.save
-  redirect '/countries'
-  else
-    erb :'countries/new'
-  end
+    #Get country from tabcomplete
 end
+
+### LOGIN & SIGNUP
 
 get '/login' do
   erb :'auth/login'
@@ -37,14 +29,24 @@ get '/signup' do
 end
 
 post '/login' do
-  @user = User.find_by(username: params[:username], email: params[:email], password: params[:password])
+  @user = User.find_by(username: params[:username], password: params[:password])
 
-  if @user.save
+  if @user
     session[:user_id] = @user.id
-    redirect '/countries/new'
+    redirect '/profile'
   else
     erb :'auth/login'
   end
+end
+
+get '/profile' do
+  @current_user = current_user
+  erb :'profile/index'
+end
+
+get '/profile/:id' do
+  @current_user = User.find(:id)
+  erb :'profile/index'
 end
 
 post '/signup' do
@@ -53,10 +55,16 @@ post '/signup' do
     email: params[:email],
     password: params[:password]
   )
+
   if @user.save
     session[:user_id] = @user.id
-    redirect '/countries/new'
+    redirect '/profile'
   else
     erb :'auth/signup'
   end
+end
+
+post '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
